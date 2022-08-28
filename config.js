3318d65128599cb56dff5d49c6c42c79821dcb52
shaxboz19 <shaxboz.nv@mail.ru>
2022-08-28 16:32:01 +0500

    start

---
+++ config.js
@@ -0,0 +1,3 @@
+export const ALLOWED_ORIGIN = 'http://localhost:3000'
+
+export const MONGODB_URI = 'mongodb://localhost:27017/chat'
---
+++ socket_io/handlers/user.handlers.js
@@ -0,0 +1,54 @@
+// "хранилище" пользователей
+const users = {}
+
+export default function userHandlers(io, socket) {
+  // извлекаем идентификатор комнаты и имя пользователя из объекта сокета
+  const { roomId, userName } = socket
+
+  // инициализируем хранилище пользователей
+  if (!users[roomId]) {
+    users[roomId] = []
+  }
+
+  // утилита для обновления списка пользователей
+  const updateUserList = () => {
+    // сообщение получают только пользователи, находящиеся в комнате
+    io.to(roomId).emit('user_list:update', users[roomId])
+  }
+  socket.on('user:get', async () => {
+    updateUserList()
+  })
+
+  // обрабатываем подключение нового пользователя
+  socket.on('user:add', async (user) => {
+    // сообщаем другим пользователям об этом
+    socket.to(roomId).emit('log', `User ${userName} connected`)
+
+    // записываем идентификатор сокета пользователя
+    user.socketId = socket.id
+
+    // записываем пользователя в хранилище
+    const item = users[roomId].find(i => i.id == user.id)
+    if(!item) {
+        users[roomId].push(user)
+    }
+
+
+    // обновляем список пользователей
+    updateUserList()
+  })
+
+  // обрабатываем отключения пользователя
+  socket.on('disconnect', () => {
+    if (!users[roomId]) return
+
+    // сообщаем об этом другим пользователям
+    socket.to(roomId).emit('log', `User ${userName} disconnected`)
+
+    // удаляем пользователя из хранилища
+    users[roomId] = users[roomId].filter((u) => u.socketId !== socket.id)
+
+    // обновляем список пользователей
+    updateUserList()
+  })
+}
